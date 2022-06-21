2a9094139a599cb579d773d2c9bac4f35074079a
Soudip Halder <soudip.halder@genesys.com>
2022-06-22 02:58:19 +0530

    v1.0

--- src/utils/getInteractionDetails.js
+++ src/utils/getInteractionDetails.js
@@ -1,3 +1,5 @@
+const jp = require('jsonpath')
+
 const getParticipantID = function(interactionID, platformClient,callback){
 let apiInstance = new platformClient.ConversationsApi();
 
@@ -6,27 +8,38 @@ let conversationId = interactionID; // String | conversationId
 // Get a conversation by id
 apiInstance.getAnalyticsConversationDetails(conversationId)
   .then((data) => {
-    let totalParticipant = data.participants.length;
+    //let totalParticipant = data.participants.length;
     let participantID = ""
     let queueID = ""
-    for(i=totalParticipant-1;i>=0;i--){
-        //console.log(data.participants[i].participantId+" "+data.participants[i].purpose)
-        if(data.participants[i].purpose==="agent"){
-            participantID = data.participants[i].participantId
-            break
-        }
-    }
-    for(i=totalParticipant-1;i>=0;i--){
-      //console.log(data.participants[i].participantId+" "+data.participants[i].purpose)
-      if(data.participants[i].purpose==="acd"){
-          queueID = data.participants[i].sessions[0].segments[0].queueId
-          break
+    let segments = jp.query(data, '$.participants[?(@.purpose=="agent")].sessions[-1:].segments[-1:]')
+    let participantIds = jp.query(data, '$.participants[?(@.purpose=="agent")].participantId')
+    console.log(segments)
+    console.log(participantIds.length)
+    for(let i=0;i<participantIds.length;i++){
+      if(!segments[i].disconnectType){
+        participantID = participantIds[i]
+        queueID = segments[i].queueId
       }
     }
+    // for(i=totalParticipant-1;i>=0;i--){
+    //     //console.log(data.participants[i].participantId+" "+data.participants[i].purpose)
+    //     if(data.participants[i].purpose==="agent"){
+    //         participantID = data.participants[i].participantId
+    //         break
+    //     }
+    // }
+    // for(i=totalParticipant-1;i>=0;i--){
+    //   //console.log(data.participants[i].participantId+" "+data.participants[i].purpose)
+    //   if(data.participants[i].purpose==="acd"){
+    //       queueID = data.participants[i].sessions[0].segments[0].queueId
+    //       break
+    //   }
+    // }
     const value = {
         participantID : participantID,
         queueID: queueID
     }
+    console.log(value)
     callback('',value)
   })
   .catch((err) => {
